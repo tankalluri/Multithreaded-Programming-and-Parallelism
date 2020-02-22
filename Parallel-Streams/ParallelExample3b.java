589c401efc51facb4acbc528651cb646c3f43f6a
Tanmayee Kalluri <tanmayee7@gmail.com>
2020-02-21 22:10:56 -0500

    Java Parallel Streams

---
+++ Parallel-Streams/ParallelExample3b.java
@@ -0,0 +1,25 @@
+package multithreading.assignment.ass6.q2;
+
+import java.util.stream.IntStream;
+
+public class ParallelExample3b {
+
+    public static void main(String[] args) {
+
+        System.out.println("Normal...");
+
+        IntStream range = IntStream.rangeClosed(1, 10);
+        range.forEach(x -> {
+            System.out.println("Thread : " + Thread.currentThread().getName() + ", value: " + x);
+        });
+
+        System.out.println("Parallel...");
+
+        IntStream range2 = IntStream.rangeClosed(1, 10);
+        range2.parallel().forEach(x -> {
+            System.out.println("Thread : " + Thread.currentThread().getName() + ", value: " + x);
+        });
+
+    }
+
+}
